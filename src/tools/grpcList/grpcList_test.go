3a82288550903097e617489f71cc10f990d80550
Blaise Dias <blaise.dias@mayadata.io>
2021-05-14 13:53:06 +0100

    refactor: move cleanup to tools
    Make grpcList output more user friendly by lising
    nexuses, replicas and pools  per node
    rather then for the cluster.

--- src/tools/grpcList/grpcList_test.go
+++ src/tools/grpcList/grpcList_test.go
@@ -1,6 +1,7 @@
 package cleanup
 
 import (
+	"mayastor-e2e/common/mayastorclient"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -20,29 +21,41 @@ func TestRestartMayastor(t *testing.T) {
 
 var _ = Describe("Mayastor utility: gRPC lists", func() {
 	It("should use gRPC to list mayastor nexuses, replicas and pools", func() {
-		nexuses, err := k8stest.ListNexusesInCluster()
-		if err == nil {
-			for _, nexus := range nexuses {
-				logf.Log.Info("", "nexus", nexus)
-			}
-		} else {
-			logf.Log.Info("nexuses", "error", err)
+		nodes, err := k8stest.GetNodeLocs()
+		if err != nil {
+			logf.Log.Info("list nodes failed", "error", err)
+			return
 		}
-		replicas, err := k8stest.ListReplicasInCluster()
-		if err == nil {
-			for _, replica := range replicas {
-				logf.Log.Info("", "replica", replica)
+		for _, node := range nodes {
+			if !node.MayastorNode {
+				continue
 			}
-		} else {
-			logf.Log.Info("replicas", "error", err)
-		}
-		pools, err := k8stest.ListPoolsInCluster()
-		if err == nil {
-			for _, pool := range pools {
-				logf.Log.Info("", "pool", pool)
+			addrs := []string{node.IPAddress}
+			nexuses, err := mayastorclient.ListNexuses(addrs)
+			logf.Log.Info(node.NodeName, "IP address", node.IPAddress)
+			if err == nil {
+				for _, nexus := range nexuses {
+					logf.Log.Info("", "nexus", nexus)
+				}
+			} else {
+				logf.Log.Info("nexuses", "error", err)
 			}
-		} else {
-			logf.Log.Info("pools", "error", err)
+			replicas, err := mayastorclient.ListReplicas(addrs)
+			if err == nil {
+				for _, replica := range replicas {
+					logf.Log.Info("", "replica", replica)
+				}
+			} else {
+				logf.Log.Info("replicas", "error", err)
+			}
+			pools, err := mayastorclient.ListPools(addrs)
+			if err == nil {
+				for _, pool := range pools {
+					logf.Log.Info("", "pool", pool)
+				}
+			} else {
+				logf.Log.Info("pools", "error", err)
+			}
 		}
 	})
 })
