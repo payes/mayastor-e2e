d99300c84a917a7e829efbde2b875f362787c2d3
Blaise Dias <blaise.dias@mayadata.io>
2021-06-01 18:58:26 +0100

    refactor: test order, swap node_failure and node_shutdown

--- scripts/test_lists.sh
+++ scripts/test_lists.sh
@@ -14,6 +14,8 @@ resource_check
 replica
 rebuild
 ms_pod_disruption"
+
+
 CONTINUOUS_TEST_LIST="
 basic_volume_io
 csi
@@ -23,12 +25,16 @@ io_soak
 volume_filesystem
 ms_pod_disruption
 ms_pod_disruption_no_io"
+
+
 NIGHTLY_TEST_LIST="
 basic_volume_io
 csi
 resource_check
 io_soak
 multiple_vols_pod_io"
+
+
 NIGHTLY_FULL_TEST_LIST="
 basic_volume_io
 csi
@@ -54,12 +60,16 @@ pvc_waitforfirstconsumer
 msv_rebuild
 pvc_delete
 maximum_vols_io
-node_failure
-node_shutdown"
+node_shutdown
+node_failure"
+
+
 ONDEMAND_TEST_LIST="
 basic_volume_io
 csi
 resource_check"
+
+
 SELF_CI_TEST_LIST="
 basic_volume_io
 csi
@@ -75,6 +85,8 @@ dynamic_provisioning
 mayastorpool_schema
 pvc_waitforfirstconsumer
 maximum_vols_io"
+
+
 SOAK_TEST_LIST="
 io_soak"
 
