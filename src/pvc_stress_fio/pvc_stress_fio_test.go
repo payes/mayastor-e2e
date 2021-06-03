5252e09df45afea491da6f299f2d7a98bc0991ba
Blaise Dias <blaise.dias@mayadata.io>
2021-06-03 13:37:18 +0100

    refactor: make pvc_stress_test conform with other tests

--- src/pvc_stress_fio/pvc_stress_fio_test.go
+++ src/pvc_stress_fio/pvc_stress_fio_test.go
@@ -12,29 +12,19 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	coreV1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var defTimeoutSecs = "60s"
 
 // Create Delete iterations
-var cdIterations = 100
+var cdIterations = e2e_config.GetConfig().PVCStress.CdCycles
 
 // Create Read Update Delete iterations
-var crudIterations = 100
+var crudIterations = e2e_config.GetConfig().PVCStress.CrudCycles
 
-// volume name and associated storage class name
-// parameters required by RmPVC
-type volSc struct {
-	volName string
-	scName  string
-}
-
-var podNames []string
-var volNames []volSc
+// Replica count for the test
+var replicaCount = e2e_config.GetConfig().PVCStress.Replicas
 
 // Create a PVC and verify that (also see and keep in sync with README.md#pvc_stress_fio)
 //	1. The PVC status transitions to bound,
@@ -48,86 +38,10 @@ var volNames []volSc
 func testPVC(volName string, protocol common.ShareProto, runFio bool) {
 	logf.Log.Info("testPVC", "volume", volName, "protocol", protocol, "run FIO", runFio)
 	scName := "pvc-stress-test-" + string(protocol)
-	err := k8stest.MkStorageClass(scName, e2e_config.GetConfig().BasicVolumeIO.Replicas, protocol, common.NSDefault)
+	err := k8stest.MkStorageClass(scName, replicaCount, protocol, common.NSDefault)
 	Expect(err).ToNot(HaveOccurred(), "Creating storage class %s", scName)
-	// PVC create options
-	createOpts := &coreV1.PersistentVolumeClaim{
-		ObjectMeta: metaV1.ObjectMeta{
-			Name:      volName,
-			Namespace: common.NSDefault,
-		},
-		Spec: coreV1.PersistentVolumeClaimSpec{
-			StorageClassName: &scName,
-			AccessModes:      []coreV1.PersistentVolumeAccessMode{coreV1.ReadWriteOnce},
-			Resources: coreV1.ResourceRequirements{
-				Requests: coreV1.ResourceList{
-					coreV1.ResourceStorage: resource.MustParse("64Mi"),
-				},
-			},
-		},
-	}
-	// Create the PVC.
-	_, createErr := k8stest.CreatePVC(createOpts, common.NSDefault)
-	Expect(createErr).To(BeNil())
 
-	// Confirm the PVC has been created.
-	pvc, getPvcErr := k8stest.GetPVC(volName, common.NSDefault)
-	Expect(getPvcErr).To(BeNil())
-	Expect(pvc).ToNot(BeNil())
-
-	// For cleanup
-	tmp := volSc{volName, scName}
-	volNames = append(volNames, tmp)
-
-	// Wait for the PVC to be bound.
-	Eventually(func() coreV1.PersistentVolumeClaimPhase {
-		return k8stest.GetPvcStatusPhase(volName, common.NSDefault)
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(coreV1.ClaimBound))
-
-	// Refresh the PVC contents, so that we can get the PV name.
-	pvc, getPvcErr = k8stest.GetPVC(volName, common.NSDefault)
-	Expect(getPvcErr).To(BeNil())
-	Expect(pvc).ToNot(BeNil())
-
-	// Wait for the PV to be provisioned
-	Eventually(func() *coreV1.PersistentVolume {
-		pv, getPvErr := k8stest.GetPV(pvc.Spec.VolumeName)
-		if getPvErr != nil {
-			return nil
-		}
-		return pv
-
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Not(BeNil()))
-
-	// Wait for the PV to be bound.
-	Eventually(func() coreV1.PersistentVolumePhase {
-		return k8stest.GetPvStatusPhase(pvc.Spec.VolumeName)
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(coreV1.VolumeBound))
-
-	// Wait for the MSV to be provisioned
-	Eventually(func() *k8stest.MayastorVolStatus {
-		return k8stest.GetMSV(string(pvc.ObjectMeta.UID))
-	},
-		defTimeoutSecs, //timeout
-		"1s",           // polling interval
-	).Should(Not(BeNil()))
-
-	// Wait for the MSV to be healthy
-	Eventually(func() string {
-		return k8stest.GetMsvState(string(pvc.ObjectMeta.UID))
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal("healthy"))
+	_ = k8stest.MkPVC(64, volName, scName, common.VolFileSystem, common.NSDefault)
 
 	if runFio {
 		// Create the fio Pod
@@ -135,9 +49,6 @@ func testPVC(volName string, protocol common.ShareProto, runFio bool) {
 		pod, err := k8stest.CreateFioPod(fioPodName, volName, common.VolFileSystem, common.NSDefault)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(pod).ToNot(BeNil())
-
-		// For cleanup
-		podNames = append(podNames, fioPodName)
 
 		// Wait for the fio Pod to transition to running
 		Eventually(func() bool {
@@ -154,41 +65,12 @@ func testPVC(volName string, protocol common.ShareProto, runFio bool) {
 		// Delete the fio pod
 		err = k8stest.DeletePod(fioPodName, common.NSDefault)
 		Expect(err).ToNot(HaveOccurred())
-
-		// cleanup
-		podNames = podNames[:len(podNames)-1]
 	}
 
 	// Delete the PVC
-	deleteErr := k8stest.DeletePVC(volName, common.NSDefault)
-	Expect(deleteErr).To(BeNil())
-
-	// Wait for the PVC to be deleted.
-	Eventually(func() bool {
-		return k8stest.IsPVCDeleted(volName, common.NSDefault)
-	},
-		"120s", // timeout
-		"1s",   // polling interval
-	).Should(Equal(true))
-
-	// Wait for the PV to be deleted.
-	Eventually(func() bool {
-		return k8stest.IsPVDeleted(pvc.Spec.VolumeName)
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(true))
-
-	// Wait for the MSV to be deleted.
-	Eventually(func() bool {
-		return k8stest.IsMSVDeleted(string(pvc.ObjectMeta.UID))
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(true))
+	k8stest.RmPVC(volName, scName, common.NSDefault)
 
 	// cleanup
-	volNames = volNames[:len(volNames)-1]
 	err = k8stest.RmStorageClass(scName)
 	Expect(err).ToNot(HaveOccurred(), "Deleting storage class %s", scName)
 }
@@ -202,7 +84,7 @@ func stressTestPVC(iters int, runFio bool) {
 		// Sadly we cannot enumerate over enums so we have to explicitly invoke
 		testPVC(fmt.Sprintf("stress-pvc-nvmf%s-%d", decoration, ix), common.ShareProtoNvmf, runFio)
 		// FIXME: HACK disable iSCSI tests temporarily till Mayastor is fixed.
-		//		testPVC(fmt.Sprintf("stress-pvc-iscsi%s-%d", decoration, ix), common.ShareProtoIscsi, runFio)
+		//testPVC(fmt.Sprintf("stress-pvc-iscsi%s-%d", decoration, ix), common.ShareProtoIscsi, runFio)
 	}
 }
 
@@ -226,22 +108,18 @@ var _ = Describe("Mayastor PVC Stress test", func() {
 	})
 
 	It("should stress test creation and deletion of PVCs provisioned over iSCSI and NVMe-of", func() {
+		logf.Log.Info("Number of cycles are", "Create/Delete", cdIterations)
 		stressTestPVC(cdIterations, false)
 	})
 
 	It("should stress test creation and deletion of PVCs provisioned over iSCSI and NVMe-of", func() {
+		logf.Log.Info("Number of cycles are", "Create/Read/Update/Delete", crudIterations)
 		stressTestPVC(crudIterations, true)
 	})
 })
 
 var _ = BeforeSuite(func(done Done) {
 	k8stest.SetupTestEnv()
-	e2eCfg := e2e_config.GetConfig()
-	cdIterations = e2eCfg.PVCStress.CdCycles
-	crudIterations = e2eCfg.PVCStress.CrudCycles
-
-	logf.Log.Info("Number of cycles are", "Create/Delete", cdIterations, "Create/Read/Update/Delete", crudIterations)
-
 	close(done)
 }, 60)
 
