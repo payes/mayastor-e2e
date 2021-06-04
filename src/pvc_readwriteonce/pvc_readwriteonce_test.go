5b879bfe1c1d3edf744786b83b18d134e6a7b508
Rohan Kumar <rohan.kumar@mayadata.io>
2021-06-04 15:44:35 +0530

    refactor : remove redundant code from initial tests  (#106)
    refactor pvc_readwriteonce , ms_pod_restart and pvc_delete tests

--- src/pvc_readwriteonce/pvc_readwriteonce_test.go
+++ src/pvc_readwriteonce/pvc_readwriteonce_test.go
@@ -1,7 +1,6 @@
 package pvc_readwriteonce
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -13,12 +12,13 @@ import (
 	. "github.com/onsi/gomega"
 
 	coreV1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-var defTimeoutSecs = "60s"
+const (
+	defTimeoutSecs = "60s" // in seconds
+	pvcSize        = 1024  // In Mb
+)
 
 // Create a PVC and verify that
 //	1. The PVC status transitions to bound,
@@ -29,10 +29,10 @@ var defTimeoutSecs = "60s"
 //	1. The PVC is deleted
 //	2. The associated PV is deleted
 //  3. The associated MV is deleted
-func testPVC(volName string, protocol common.ShareProto, runFio bool, fsType common.FileSystemType) {
+func testPVC(volName string, protocol common.ShareProto, fsType common.FileSystemType) {
 	params := e2e_config.GetConfig().BasicVolumeIO
 	logf.Log.Info("Test", "parameters", params)
-	logf.Log.Info("testPVC", "volume", volName, "protocol", protocol, "run FIO", runFio, "fsType", fsType)
+	logf.Log.Info("testPVC", "volume", volName, "protocol", protocol, "fsType", fsType)
 	scName := "pvc-rwo-test-" + string(protocol) + "-" + string(fsType)
 	scObj, err := k8stest.NewScBuilder().
 		WithName(scName).
@@ -42,80 +42,11 @@ func testPVC(volName string, protocol common.ShareProto, runFio bool, fsType com
 	Expect(err).ToNot(HaveOccurred(), "Generating storage class definition %s", scName)
 	err = k8stest.CreateSc(scObj)
 	Expect(err).ToNot(HaveOccurred(), "Creating storage class %s", scName)
-	// PVC create options
-	createOpts := &coreV1.PersistentVolumeClaim{
-		ObjectMeta: metaV1.ObjectMeta{
-			Name:      volName,
-			Namespace: common.NSDefault,
-		},
-		Spec: coreV1.PersistentVolumeClaimSpec{
-			StorageClassName: &scName,
-			AccessModes:      []coreV1.PersistentVolumeAccessMode{coreV1.ReadWriteOnce},
-			Resources: coreV1.ResourceRequirements{
-				Requests: coreV1.ResourceList{
-					coreV1.ResourceStorage: resource.MustParse("64Mi"),
-				},
-			},
-		},
-	}
-	// Create the PVC.
-	_, createErr := k8stest.CreatePVC(createOpts, common.NSDefault)
-	Expect(createErr).To(BeNil())
 
-	// Confirm the PVC has been created.
-	pvc, getPvcErr := k8stest.GetPVC(volName, common.NSDefault)
-	Expect(getPvcErr).To(BeNil())
-	Expect(pvc).ToNot(BeNil())
+	// create PVC
+	uid := k8stest.MkPVC(pvcSize, volName, scName, common.VolFileSystem, common.NSDefault)
+	Expect(uid).ToNot(BeNil(), "Failed to create PVC")
 
-	// Wait for the PVC to be bound.
-	Eventually(func() coreV1.PersistentVolumeClaimPhase {
-		return k8stest.GetPvcStatusPhase(volName, common.NSDefault)
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(coreV1.ClaimBound))
-
-	// Refresh the PVC contents, so that we can get the PV name.
-	pvc, getPvcErr = k8stest.GetPVC(volName, common.NSDefault)
-	Expect(getPvcErr).To(BeNil())
-	Expect(pvc).ToNot(BeNil())
-
-	// Wait for the PV to be provisioned
-	Eventually(func() *coreV1.PersistentVolume {
-		pv, getPvErr := k8stest.GetPV(pvc.Spec.VolumeName)
-		if getPvErr != nil {
-			return nil
-		}
-		return pv
-
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Not(BeNil()))
-
-	// Wait for the PV to be bound.
-	Eventually(func() coreV1.PersistentVolumePhase {
-		return k8stest.GetPvStatusPhase(pvc.Spec.VolumeName)
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(coreV1.VolumeBound))
-
-	// Wait for the MSV to be provisioned
-	Eventually(func() *k8stest.MayastorVolStatus {
-		return k8stest.GetMSV(string(pvc.ObjectMeta.UID))
-	},
-		defTimeoutSecs, //timeout
-		"1s",           // polling interval
-	).Should(Not(BeNil()))
-
-	// Wait for the MSV to be healthy
-	Eventually(func() string {
-		return k8stest.GetMsvState(string(pvc.ObjectMeta.UID))
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal("healthy"))
 	// list all nodes
 	nodes, err := k8stest.GetNodeLocs()
 	Expect(err).ToNot(HaveOccurred())
@@ -127,138 +58,106 @@ func testPVC(volName string, protocol common.ShareProto, runFio bool, fsType com
 			workerNodes = append(workerNodes, node.NodeName)
 		}
 	}
-	if runFio {
-		// Create the fio Pod on first worker node
-		fioPodFirstNodeName := "fio-" + volName + "-" + workerNodes[0]
-		// fio pod labels
-		label := map[string]string{
-			"app": "fio",
-		}
-		// fio pod container
-		firstPodContainer := coreV1.Container{
-			Name:            fioPodFirstNodeName,
-			Image:           common.GetFioImage(),
-			ImagePullPolicy: coreV1.PullAlways,
-			Args:            []string{"sleep", "1000000"},
-		}
-
-		// volume claim details
-		volume := coreV1.Volume{
-			Name: "ms-volume",
-			VolumeSource: coreV1.VolumeSource{
-				PersistentVolumeClaim: &coreV1.PersistentVolumeClaimVolumeSource{
-					ClaimName: volName,
-				},
-			},
-		}
-
-		podObj, err := k8stest.NewPodBuilder().
-			WithName(fioPodFirstNodeName).
-			WithNamespace(common.NSDefault).
-			WithNodeSelectorHostnameNew(workerNodes[0]).
-			WithRestartPolicy(coreV1.RestartPolicyNever).
-			WithContainer(firstPodContainer).
-			WithVolume(volume).
-			WithVolumeDeviceOrMount(common.VolFileSystem).
-			WithLabels(label).Build()
-		Expect(err).ToNot(HaveOccurred(), "Generating fio pod definition %s", fioPodFirstNodeName)
-		Expect(podObj).ToNot(BeNil())
-		// Create first fio pod
-		_, err = k8stest.CreatePod(podObj, common.NSDefault)
-		Expect(err).ToNot(HaveOccurred(), "Creating fio pod %s", fioPodFirstNodeName)
-		// Wait for the fio Pod to transition to running
-		Eventually(func() bool {
-			return k8stest.IsPodRunning(fioPodFirstNodeName, common.NSDefault)
-		},
-			defTimeoutSecs,
-			"1s",
-		).Should(Equal(true))
-
-		// Run the fio test
-		_, err = k8stest.RunFio(fioPodFirstNodeName, 5, common.FioFsFilename, common.DefaultFioSizeMb)
-		Expect(err).ToNot(HaveOccurred())
-
-		// Create the fio Pod on second worker node which will consume same pvc
-		fioPodSecNodeName := "fio-" + volName + "-" + workerNodes[1]
-
-		//Update pod name while creating second fio pod
-		podObj.Name = fioPodSecNodeName
-
-		// Update nodeselector for second node
-		podObj.Spec.NodeSelector = map[string]string{
-			k8stest.K8sNodeLabelKeyHostname: workerNodes[1],
-		}
-
-		// Create second fio pod
-		_, err = k8stest.CreatePod(podObj, common.NSDefault)
-		Expect(err).ToNot(HaveOccurred(), "Creating fio pod %s", fioPodSecNodeName)
-		logf.Log.Info("Checking fio pod status", "timeout", params.FioTimeout)
-		tSecs := 0
-		var phase coreV1.PodPhase
-		for {
-			if tSecs > params.FioTimeout {
-				break
-			}
-			time.Sleep(1 * time.Second)
-			tSecs += 1
-			phase, err = k8stest.CheckPodCompleted(fioPodSecNodeName, common.NSDefault)
-			Expect(err).To(BeNil(), "CheckPodComplete got error %s", err)
-			if phase != coreV1.PodPending {
-				break
-			}
-		}
-		Expect(phase == coreV1.PodPending).To(BeTrue(), "fio pod phase is %s", phase)
-
-		// Delete the fio pod scheduled on first node
-		err = k8stest.DeletePod(fioPodFirstNodeName, common.NSDefault)
-		Expect(err).ToNot(HaveOccurred())
-
-		// Delete the fio pod scheduled on second node
-		err = k8stest.DeletePod(fioPodSecNodeName, common.NSDefault)
-		Expect(err).ToNot(HaveOccurred())
-
+	// Create the fio Pod on first worker node
+	fioPodFirstNodeName := "fio-" + volName + "-" + workerNodes[0]
+	// fio pod labels
+	label := map[string]string{
+		"app": "fio",
+	}
+	// fio pod container
+	firstPodContainer := coreV1.Container{
+		Name:            fioPodFirstNodeName,
+		Image:           common.GetFioImage(),
+		ImagePullPolicy: coreV1.PullAlways,
+		Args:            []string{"sleep", "1000000"},
 	}
 
+	// volume claim details
+	volume := coreV1.Volume{
+		Name: "ms-volume",
+		VolumeSource: coreV1.VolumeSource{
+			PersistentVolumeClaim: &coreV1.PersistentVolumeClaimVolumeSource{
+				ClaimName: volName,
+			},
+		},
+	}
+
+	podObj, err := k8stest.NewPodBuilder().
+		WithName(fioPodFirstNodeName).
+		WithNamespace(common.NSDefault).
+		WithNodeSelectorHostnameNew(workerNodes[0]).
+		WithRestartPolicy(coreV1.RestartPolicyNever).
+		WithContainer(firstPodContainer).
+		WithVolume(volume).
+		WithVolumeDeviceOrMount(common.VolFileSystem).
+		WithLabels(label).Build()
+	Expect(err).ToNot(HaveOccurred(), "Generating fio pod definition %s", fioPodFirstNodeName)
+	Expect(podObj).ToNot(BeNil())
+	// Create first fio pod
+	_, err = k8stest.CreatePod(podObj, common.NSDefault)
+	Expect(err).ToNot(HaveOccurred(), "Creating fio pod %s", fioPodFirstNodeName)
+	// Wait for the fio Pod to transition to running
+	Eventually(func() bool {
+		return k8stest.IsPodRunning(fioPodFirstNodeName, common.NSDefault)
+	},
+		defTimeoutSecs,
+		"1s",
+	).Should(Equal(true))
+
+	// Run the fio test
+	_, err = k8stest.RunFio(fioPodFirstNodeName, 5, common.FioFsFilename, common.DefaultFioSizeMb)
+	Expect(err).ToNot(HaveOccurred())
+
+	// Create the fio Pod on second worker node which will consume same pvc
+	fioPodSecNodeName := "fio-" + volName + "-" + workerNodes[1]
+
+	//Update pod name while creating second fio pod
+	podObj.Name = fioPodSecNodeName
+
+	// Update nodeselector for second node
+	podObj.Spec.NodeSelector = map[string]string{
+		k8stest.K8sNodeLabelKeyHostname: workerNodes[1],
+	}
+
+	// Create second fio pod
+	_, err = k8stest.CreatePod(podObj, common.NSDefault)
+	Expect(err).ToNot(HaveOccurred(), "Creating fio pod %s", fioPodSecNodeName)
+	logf.Log.Info("Checking fio pod status", "timeout", params.FioTimeout)
+	tSecs := 0
+	var phase coreV1.PodPhase
+	for {
+		if tSecs > params.FioTimeout {
+			break
+		}
+		time.Sleep(1 * time.Second)
+		tSecs += 1
+		phase, err = k8stest.CheckPodCompleted(fioPodSecNodeName, common.NSDefault)
+		Expect(err).To(BeNil(), "CheckPodComplete got error %s", err)
+		if phase != coreV1.PodPending {
+			break
+		}
+	}
+	Expect(phase == coreV1.PodPending).To(BeTrue(), "fio pod phase is %s", phase)
+
+	// Delete the fio pod scheduled on first node
+	err = k8stest.DeletePod(fioPodFirstNodeName, common.NSDefault)
+	Expect(err).ToNot(HaveOccurred())
+
+	// Delete the fio pod scheduled on second node
+	err = k8stest.DeletePod(fioPodSecNodeName, common.NSDefault)
+	Expect(err).ToNot(HaveOccurred())
+
 	// Delete the PVC
-	deleteErr := k8stest.DeletePVC(volName, common.NSDefault)
-	Expect(deleteErr).To(BeNil())
-
-	// Wait for the PVC to be deleted.
-	Eventually(func() bool {
-		return k8stest.IsPVCDeleted(volName, common.NSDefault)
-	},
-		"120s", // timeout
-		"1s",   // polling interval
-	).Should(Equal(true))
-
-	// Wait for the PV to be deleted.
-	Eventually(func() bool {
-		return k8stest.IsPVDeleted(pvc.Spec.VolumeName)
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(true))
-
-	// Wait for the MSV to be deleted.
-	Eventually(func() bool {
-		return k8stest.IsMSVDeleted(string(pvc.ObjectMeta.UID))
-	},
-		defTimeoutSecs, // timeout
-		"1s",           // polling interval
-	).Should(Equal(true))
+	k8stest.RmPVC(volName, scName, common.NSDefault)
 
 	err = k8stest.RmStorageClass(scName)
 	Expect(err).ToNot(HaveOccurred(), "Deleting storage class %s", scName)
 }
 
-func readwriteonceTestPVC(runFio bool, fsType common.FileSystemType) {
-	decoration := ""
-	if runFio {
-		decoration = "-io"
-	}
+func readwriteonceTestPVC(fsType common.FileSystemType) {
 
 	// Sadly we cannot enumerate over enums so we have to explicitly invoke
-	testPVC(fmt.Sprintf("rwo-pvc-nvmf%s", decoration), common.ShareProtoNvmf, runFio, fsType)
+	testPVC("rwo-pvc-nvmf-io", common.ShareProtoNvmf, fsType)
 	// FIXME: HACK disable iSCSI tests temporarily till Mayastor is fixed.
 	//	testPVC(fmt.Sprintf("rwo-pvc-iscsi%s", decoration), common.ShareProtoIscsi, runFio, fsType)
 
@@ -284,10 +183,10 @@ var _ = Describe("Mayastor PVC ReadWriteOnce test", func() {
 	})
 
 	It("should readwriteonce test creation and deletion of PVCs having ext4 as fsType provisioned over iSCSI and NVMe-of", func() {
-		readwriteonceTestPVC(true, common.Ext4FsType)
+		readwriteonceTestPVC(common.Ext4FsType)
 	})
 	It("should readwriteonce test creation and deletion of PVCs having xfs as fsType provisioned over iSCSI and NVMe-of", func() {
-		readwriteonceTestPVC(true, common.XfsFsType)
+		readwriteonceTestPVC(common.XfsFsType)
 	})
 
 })
