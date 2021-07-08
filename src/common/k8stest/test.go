ffea90ff01f1aa75d8d8842aac3429f92ecfe5c0
Blaise Dias <blaise.dias@mayadata.io>
2021-07-08 07:35:57 +0100

    fix: update error message for more clarity
    Change the error message to
    - not running test case, k8s cluster is not "clean"
    From
    - unclean test cluster!!!

--- src/common/k8stest/test.go
+++ src/common/k8stest/test.go
@@ -244,7 +244,7 @@ func BeforeEachCheck() error {
 			logf.Log.Info("BeforeEachCheck failed", "error", err)
 		}
 	}
-	return fmt.Errorf("unclean test cluster!!!\n%v", err)
+	return fmt.Errorf("not running test case, k8s cluster is not \"clean\"!!!\n%v", err)
 }
 
 // AfterEachCheck asserts that the state of mayastor resources has been restored.
