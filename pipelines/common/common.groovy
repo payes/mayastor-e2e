ce7c8fefaaa95aade00f70c702fbfc1871368840
chriswldenyer <christopher.denyer@mayadata.io>
2021-07-12 15:21:55 +0100

    test(e2e): mq-1569 uuid for BuildCluster
    Run BuildCluster with a unique random uuid
    to ensure Jenkins treats each request to run the
    pipeline separately, and so results in
    one new job per request.

--- pipelines/common/common.groovy
+++ pipelines/common/common.groovy
@@ -83,15 +83,26 @@ def BuildImages(mayastorBranch, moacBranch, test_tag) {
 }
 
 def BuildCluster(e2e_build_cluster_job, e2e_environment) {
+  def uuid = sh(
+    script: 'printf $(uuidgen -r)',
+    returnStdout: true
+  )
   return build(
     job: "${e2e_build_cluster_job}",
     propagate: true,
     wait: true,
-    parameters: [[
-      $class: 'StringParameterValue',
-      name: "ENVIRONMENT",
-      value: "${e2e_environment}"
-    ]]
+    parameters: [
+      [
+        $class: 'StringParameterValue',
+        name: "ENVIRONMENT",
+        value: "${e2e_environment}"
+      ],
+      [
+        $class: 'StringParameterValue',
+        name: "UUID",
+        value: "${uuid}"
+      ]
+    ]
   )
 }
 
