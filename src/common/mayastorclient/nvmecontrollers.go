08e9749fc98147d7468aecb22ee49ae7c00b043d
Blaise Dias <blaise.dias@mayadata.io>
2021-07-13 16:36:01 +0100

    bump up timeout for nvme controller gRPC call

--- src/common/mayastorclient/nvmecontrollers.go
+++ src/common/mayastorclient/nvmecontrollers.go
@@ -44,7 +44,7 @@ func listNvmeController(address string) ([]NvmeController, error) {
 	}(conn)
 
 	c := mayastorGrpc.NewMayastorClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 
 	response, err := c.ListNvmeControllers(ctx, &null)
