58d6780d939685091664564218974c330eda7fac
prabhatkumarthakur <prabhat.thakur@mayadata.io>
2021-08-06 14:12:12 +0530

    This PR make sure that mayastor pod is in ready state to accept the GRPC request and then continue with next step of test case. Earlier in test case mayastor pod was not waiting to come in a state where it can accept the grpc request so the test case was failing with connection timeout error

--- scripts/test_lists.sh
+++ scripts/test_lists.sh
@@ -44,7 +44,6 @@ io_soak
 pvc_delete
 ms_pod_disruption
 ms_pod_disruption_no_io
-ms_pod_disruption_rm_msv
 maximum_vols_io
 multiple_vols_pod_io
 node_shutdown
@@ -93,4 +92,5 @@ msv_rebuild
 node_failure
 primitive_msp_state
 single_msn_shutdown
+ms_pod_disruption_rm_msv
 "
