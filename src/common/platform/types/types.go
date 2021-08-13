02a0a57192a9e5f3eb42718544341998f548a986
Payes Anand <payes.anand@mayadata.io>
2021-08-13 20:38:25 +0530

    add primitive_device_retirement tests
    Signed-off-by: Payes Anand <payes.anand@mayadata.io>

--- src/common/platform/types/types.go
+++ src/common/platform/types/types.go
@@ -3,5 +3,8 @@ package types
 type Platform interface {
 	PowerOnNode(node string) error
 	PowerOffNode(node string) error
+	RebootNode(node string) error
 	GetNodeStatus(node string) (string, error)
+	DetachVolume(volName string) error
+	AttachVolume(volName, node string) error
 }
