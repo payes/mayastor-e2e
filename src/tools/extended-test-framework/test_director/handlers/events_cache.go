6069ac948862121e76167a1a3daf77656344c4b1
Milan Hajek <milan.hajek@mayadata.io>
2021-09-22 16:30:24 +0200

    slack notifications for post event

--- src/tools/extended-test-framework/test_director/handlers/events_cache.go
+++ src/tools/extended-test-framework/test_director/handlers/events_cache.go
@@ -2,7 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
+	"test-director/connectors"
 	"test-director/models"
+	"time"
 
 	"github.com/patrickmn/go-cache"
 )
@@ -36,6 +39,7 @@ func (r *EventCache) GetAll() []*models.Event {
 }
 
 func (r *EventCache) Set(key string, data models.Event) error {
+	setupSlackNotification(&data.EventSpec)
 	b, err := json.Marshal(data)
 	if err != nil {
 		return err
@@ -44,11 +48,34 @@ func (r *EventCache) Set(key string, data models.Event) error {
 	return nil
 }
 
-
-
 func InitEventCache() {
 	eventInterface = &EventCache{
 		client: cache.New(-1, 0),
 	}
 }
 
+func setupSlackNotification(data *models.EventSpec) {
+	sc := connectors.SlackClient{
+		WebHookUrl: "https://hooks.slack.com/services/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",
+		UserName:   string(*data.SourceClass),
+		Channel:    "#test_director",
+		TimeOut:    10 * time.Second,
+	}
+	sn := connectors.SlackJobNotification{
+		Details:   *data.Message,
+		IconEmoji: ":ghost:",
+		Text:      string(*data.Class) + " - " + *data.SourceInstance,
+	}
+	switch *data.Class {
+	case models.EventClassEnumFAIL:
+		sn.Color = "danger"
+	case models.EventClassEnumWARN:
+		sn.Color = "warning"
+	default:
+		sn.Color = "good"
+	}
+	err := sc.SendJobNotification(sn)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+}
