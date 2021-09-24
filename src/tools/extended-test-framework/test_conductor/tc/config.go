302434930160ae70f9c5163ecadb562675e284ba
chriswldenyer <christopher.denyer@mayadata.io>
2021-09-24 22:09:32 +0100

    test(e2e): use configmap

--- src/tools/extended-test-framework/test_conductor/tc/config.go
+++ src/tools/extended-test-framework/test_conductor/tc/config.go
@@ -25,21 +25,10 @@ type ExtendedTestConfig struct {
 	ImageTag string `yaml:"imageTag" env:"e2e_image_tag"`
 
 	// FIXME: handle empty poolDevice
-	PoolDevice  string `yaml:"poolDevice" env:"e2e_pool_device"`
-	E2eFioImage string `yaml:"e2eFioImage" env-default:"mayadata/e2e-fio" env:"e2e_fio_image"`
-	E2eFsxImage string `yaml:"e2eFsxImage" env-default:"mayadata/e2e-fsx" env:"e2e_fsx_image"`
-	// This is an advisory setting for individual tests
-	// If set to true - typically during test development - tests with multiple It clauses should defer asserts till after
-	// resources have been cleaned up . This behaviour makes it possible to have useful runs for all It clauses.
-	// Typically set to false for CI test execution - no cleanup after first failure, as a result subsequent It clauses
-	// in the test will fail the BeforeEach check, rendering post-mortem checks on the cluster more useful.
-	// It may be set to true for when we want maximum test coverage, and post-mortem analysis is a secondary requirement.
-	// NOTE: Only some tests support this feature.
-	DefaultReplicaCount int `yaml:"defaultReplicaCount" env-default:"2" env:"e2e_default_replica_count"`
-	// Timeout for MOAC CR state reconciliation in seconds, some CR state is not update promptly for example pool usage
-	// and finalizers. On hcloud the time lag between synchronisation has been observed to be in the order of
-	// a minute.
-	MoacSyncTimeoutSeconds int `yaml:"moacSyncTimeoutSeconds" env-default:"600"`
+	PoolDevice   string `yaml:"poolDevice" env:"e2e_pool_device"`
+	E2eFioImage  string `yaml:"e2eFioImage" env-default:"mayadata/e2e-fio" env:"e2e_fio_image"`
+	ReplicaCount int    `yaml:"replicaCount" env-default:"2" env:"e2e_default_replica_count"`
+	Test         string `yaml:"test" env:"e2e_test"`
 
 	// Individual Test parameters
 	SteadyStateTest struct {
@@ -47,24 +36,7 @@ type ExtendedTestConfig struct {
 		Duration string `yaml:"duration" env-default:"60m"`
 		// Number of volumes for each mayastor instance
 		// volumes for disruptor pods are allocated from within this "pool"
-		LoadFactor int      `yaml:"loadFactor" env-default:"10"`
-		Protocols  []string `yaml:"protocols" env-default:"nvmf"`
-		// FioStartDelay units are seconds
-		FioStartDelay int    `yaml:"fioStartDelay" env-default:"90"`
-		ReadyTimeout  string `yaml:"readyTimeout" env-default:"600s"`
-		Disrupt       struct {
-			// Number of disruptor pods.
-			PodCount int `yaml:"podCount" env-default:"3"`
-			// FaultAfter units are seconds
-			FaultAfter   int    `yaml:"faultAfter" env-default:"51"`
-			ReadyTimeout string `yaml:"readyTimeout" env-default:"180s"`
-		} `yaml:"disrupt"`
-		FioDutyCycles []struct {
-			// ThinkTime units are microseconds
-			ThinkTime       int `yaml:"thinkTime"`
-			ThinkTimeBlocks int `yaml:"thinkTimeBlocks"`
-		} `yaml:"fioDutyCycles"`
-	} `yaml:"ioSoakTest"`
+	} `yaml:"steadyStateTest"`
 }
 
 var once sync.Once
@@ -80,17 +52,6 @@ func GetConfig() (ExtendedTestConfig, error) {
 
 		// Initialise the configuration
 		_ = cleanenv.ReadEnv(&gConfig)
-		gConfig.SteadyStateTest.FioDutyCycles = []struct {
-			ThinkTime       int `yaml:"thinkTime"`
-			ThinkTimeBlocks int `yaml:"thinkTimeBlocks"`
-		}{
-			{500000, 1000},
-			{750000, 1000},
-			{1250000, 2000},
-			{1500000, 3000},
-			{1750000, 3000},
-			{2000000, 4000},
-		}
 
 		// We absorb the complexity of locating the configuration file here
 		// so that scripts invoking the tests can be simpler
