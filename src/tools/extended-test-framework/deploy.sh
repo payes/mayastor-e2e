85c733802ca7e45137f8e5eba4c7e043d26997e5
chriswldenyer <christopher.denyer@mayadata.io>
2021-09-24 22:34:54 +0100

    test(e2e): missing files

---
+++ src/tools/extended-test-framework/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPTDIR=$(dirname "$(realpath "$0")")
+kubectl create -f ${SCRIPTDIR}/test_namespace.yaml
+kubectl create configmap etfw-config -n mayastor-e2e --from-file=${SCRIPTDIR}/test_conductor/config.yaml
+kubectl create -f ${SCRIPTDIR}/test_conductor.yaml
+kubectl create -f ${SCRIPTDIR}/test_director_m.yaml
+kubectl create -f ${SCRIPTDIR}/workload_monitor.yaml
+
