e249fec6e8016e58af02054ecec948ce7a8fd19b
chriswldenyer <christopher.denyer@mayadata.io>
2021-09-27 17:32:54 +0100

    test(e2e): do not generate code

--- src/tools/extended-test-framework/test_conductor/build.sh
+++ src/tools/extended-test-framework/test_conductor/build.sh
@@ -5,15 +5,7 @@ set -e pipefail
 SCRIPT_DIR=$(dirname "$0")
 cd ${SCRIPT_DIR}
 
-./gen_client_code.sh
-
-go get github.com/go-openapi/errors
-go get github.com/go-openapi/runtime
-go get github.com/go-openapi/runtime/client
-go get github.com/go-openapi/strfmt
-go get github.com/go-openapi/validate
-
-go get github.com/jessevdk/go-flags
+#./gen_client_code.sh
 
 CGO_ENABLED=0 go build -a -installsuffix cgo
 
--- src/tools/extended-test-framework/workload_monitor/build.sh
+++ src/tools/extended-test-framework/workload_monitor/build.sh
@@ -5,8 +5,8 @@ set -e pipefail
 SCRIPT_DIR=$(dirname "$0")
 cd ${SCRIPT_DIR}
 
-./gen_server_code.sh
-./gen_client_code.sh
+#./gen_server_code.sh
+#./gen_client_code.sh
 
 CGO_ENABLED=0 go build -a -installsuffix cgo
 
