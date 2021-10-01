e27964b7170522ae99811579477867a966fc442d
Rohan Kumar <rohan.kumar@mayadata.io>
2021-10-01 17:58:09 +0530

    chore: migrate tests from nightly-stable and C1 to HC1 (#278)
    * migrate tests from nightly-stable and C1 to HC1

--- scripts/test_lists.sh
+++ scripts/test_lists.sh
@@ -102,14 +102,38 @@ stale_msp_after_node_power_failure
 
 #hc1-nightly version of nightly-stable for CP2
 profiles[hc1-nightly]="
+basic_volume_io
+control_plane_rescheduling
 csi
-mayastorpool_schema
-pool_modify
-primitive_msp_deletion
-primitive_msp_state
-primitive_msp_stress
-primitive_replicas
 resource_check
+dynamic_provisioning
+expand_msp_disk
+io_soak
+mayastorpool_schema
+MQ-1783-fsx_ext4_stress
+ms_pod_disruption_rm_msv
+ms_pod_restart
+ms_pod_disruption
+ms_pod_disruption_no_io
+ms_pool_delete
+msv_rebuild
+multiple_vols_pod_io
+node_shutdown
+nexus_location
+pool_modify
+primitive_data_integrity
+primitive_fault_injection
+primitive_msp_stress
+primitive_msp_deletion
+primitive_replicas
+primitive_volumes
+pvc_delete
+pvc_readwriteonce
+pvc_stress_fio
+pvc_waitforfirstconsumer
+single_msn_shutdown
+synchronous_replication
+volume_filesystem
 "
 #stale_msp_after_node_power_failure is moac specific
 
