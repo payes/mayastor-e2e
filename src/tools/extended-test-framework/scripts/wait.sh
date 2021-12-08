6c028c4bf480da649a1ea37f8d213144a3eed94f
chriswldenyer <60256267+chriswldenyer@users.noreply.github.com>
2021-12-08 14:54:09 +0000

    test(etfw): stream test-conductor o/p (#385)
    To aid checking the progress of a test run
    we can use the kubectl log --follow option to
    stream the test-conductor output to the wait.sh script.

--- src/tools/extended-test-framework/scripts/wait.sh
+++ src/tools/extended-test-framework/scripts/wait.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-# wait 100s for test-conductor to be created
+# wait 100s for test-conductor to be running
 for i in {0..100}; do
-    kubectl get pod -n mayastor-e2e | grep test-conductor
+    kubectl get pod -n mayastor-e2e | grep test-conductor | grep "Running"
 	if [ "$?" == "0" ]; then
         break
     fi
@@ -13,21 +13,9 @@ for i in {0..100}; do
 	sleep 1
 done
 
-# minutes in 15 days. We expect the test_conductor to run for no more that 14 days
-LIMIT=$(( 60 * 24 * 15))
-echo "waiting for test-conductor to complete"
+set -e
 
-for ((i=0; i<=${LIMIT}; i++)); do
-	kubectl get pod -n mayastor-e2e test-conductor | grep 'Completed\|Error'
-	if [ "$?" == "0" ]; then
-		break
-	fi
-    if [ "$i" == "${LIMIT}" ]; then
-		echo "timed out"
-        exit 1
-    fi
-	sleep 60
-done
+timeout 15d kubectl logs -n mayastor-e2e test-conductor --follow
 
 echo "test completed"
 
