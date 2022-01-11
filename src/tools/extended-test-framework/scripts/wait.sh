d805fd16daaaec0db6b1f900541bf2a9544ebdec
chriswldenyer <60256267+chriswldenyer@users.noreply.github.com>
2022-01-11 08:36:36 +0000

    test(etfw): handle timouts in kubectl log -f (#400)

--- src/tools/extended-test-framework/scripts/wait.sh
+++ src/tools/extended-test-framework/scripts/wait.sh
@@ -1,21 +1,39 @@
 #!/usr/bin/env bash
 
+set -e
+NAMESPACE="mayastor-e2e"
 
 # wait 100s for test-conductor to be running
 for i in {0..100}; do
-    kubectl get pod -n mayastor-e2e | grep test-conductor | grep "Running"
-	if [ "$?" == "0" ]; then
+    res=$(kubectl get pod -n ${NAMESPACE} | grep test-conductor | grep 'Running\|Completed' || true)
+    if [ "${res}" != "" ]; then
         break
     fi
-	if [ "$i" == "100" ]; then
-		exit 1
-	fi
-	sleep 1
+    echo waiting
+    if [ "$i" == "100" ]; then
+        echo "timed out waiting for test-conductor"
+        exit 1
+    fi
+    sleep 1
 done
 
-set -e
-
-timeout 15d kubectl logs -n mayastor-e2e test-conductor --follow
-
-echo "test completed"
-
+SINCEOPT=""
+while [ true ]; do
+    kubectl logs -n mayastor-e2e test-conductor --follow ${SINCEOPT}
+    # If there is no output for 5 mins, logs --follow will timeout.
+    # Found that the command may timeout after 4 hours even with output.
+    # If the test-conductor is still running then re-stream the log by repeating the loop.
+    # The "--since=1m" should mean we don't lose any output but we may get some duplicated lines.
+    res=$(kubectl get pod -n ${NAMESPACE} | grep test-conductor | grep 'Running' || true)
+    if [ "${res}" == "" ]; then
+        break
+    fi
+    echo "============== output timeout - restreaming ==============="
+    SINCEOPT='--since=1m'
+done
+res=$(kubectl get pod -n ${NAMESPACE} | grep test-conductor | grep 'Completed' || true)
+if [ "${res}" == "" ]; then
+    echo "test conductor has not completed successfully"
+    exit 1
+fi
+echo "test conductor has completed"
