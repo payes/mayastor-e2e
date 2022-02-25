3c36d2807b76c48148a9874ce8662e5e1bff24e0
Blaise Dias <blaise.dias@mayadata.io>
2022-02-25 18:28:09 +0000

    testing change: run minimal test profile

--- pipelines/ubuntu-20.04-lts/ubuntu.groovy
+++ pipelines/ubuntu-20.04-lts/ubuntu.groovy
@@ -1,14 +1,19 @@
 #!/usr/bin/env groovy
 
-def GetDefaultJobParameters () {
+def GetDefaultJobParameters (k8sv, build_images, image_tag ) {
     return [
             [ $class: 'StringParameterValue',  name: "build_prefix",       value: "develop" ],
             [ $class: 'StringParameterValue',  name: "mayastorBranch",     value: "develop" ],
             [ $class: 'StringParameterValue',  name: "mcpBranch",          value: "develop" ],
             [ $class: 'StringParameterValue',  name: "xray_test_plan",     value: "MQ-2743" ],
-            [ $class: 'BooleanParameterValue', name: "xray_send_report",   value: true ],
-            [ $class: 'StringParameterValue',  name: "test_profile",       value: "regression" ]
-            [ $class: 'StringParameterValue',  name: "parallelism",        value: "10" ]
+//            [ $class: 'BooleanParameterValue', name: "xray_send_report",   value: true ],
+//            [ $class: 'StringParameterValue',  name: "test_profile",       value: "regression" ],
+            [ $class: 'BooleanParameterValue', name: "xray_send_report",   value: false ],
+            [ $class: 'StringParameterValue',  name: "test_profile",       value: "minimal" ],
+            [ $class: 'StringParameterValue',  name: "parallelism",        value: "10" ],
+            [ $class: 'StringParameterValue',   name: "kubernetes_version", value: k8sv ],
+            [ $class: 'BooleanParameterValue',  name: "build_images",       value: build_images ],
+            [ $class: 'StringParameterValue',   name: "image_tag",          value: image_tag ]
     ]
 }
 
