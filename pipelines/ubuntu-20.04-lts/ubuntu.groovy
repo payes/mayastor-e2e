3bc546c529d75c54e7062629e7fccc2945296a63
Blaise Dias <blaise.dias@mayadata.io>
2022-03-03 18:07:06 +0000

    temp

--- pipelines/ubuntu-20.04-lts/ubuntu.groovy
+++ pipelines/ubuntu-20.04-lts/ubuntu.groovy
@@ -11,8 +11,12 @@ def GetDefaultJobParameters (k8sv, params) {
             [ $class: 'StringParameterValue',  name: "dataplaneBranch",    value: "develop" ],
             [ $class: 'StringParameterValue',  name: "controlplaneBranch", value: "develop" ],
             [ $class: 'StringParameterValue',  name: "xray_test_plan",     value: params.xray_test_plan ],
-            [ $class: 'BooleanParameterValue', name: "xray_send_report",   value: true ],
-            [ $class: 'StringParameterValue',  name: "test_profile",       value: "regression" ],
+//DO NOT MERGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {
+            [ $class: 'BooleanParameterValue', name: "xray_send_report",   value: false ],
+            [ $class: 'StringParameterValue',  name: "test_profile",       value: "smoke-test" ],
+//            [ $class: 'BooleanParameterValue', name: "xray_send_report",   value: true ],
+//            [ $class: 'StringParameterValue',  name: "test_profile",       value: "regression" ],
+//}
             [ $class: 'StringParameterValue',  name: "parallelism",        value: "10" ],
             [ $class: 'StringParameterValue',  name: "kubernetes_version", value: k8sv ],
             [ $class: 'BooleanParameterValue', name: "build_images",       value: params.build_images ],
