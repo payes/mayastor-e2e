d0552b61280df794fd1ca973648c5aeb1aff1409
chriswldenyer <christopher.denyer@mayadata.io>
2022-03-09 14:54:28 +0000

    test(etfw) test director hardening

--- src/tools/extended-test-framework/test_director/connectors/xray/test_execution.go
+++ src/tools/extended-test-framework/test_director/connectors/xray/test_execution.go
@@ -2,30 +2,39 @@ package xray
 
 import (
 	"fmt"
-	"github.com/tidwall/gjson"
 	"strings"
 	"test-director/models"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
 )
 
-func CreateTestExecution(testPlanId, testId, testJiraKey string) string {
+func CreateTestExecution(testPlanId, testId, testJiraKey string) (string, error) {
 	s := fmt.Sprintf(
 		`mutation{createTestExecution(testIssueIds:["%s"] jira: {fields: {summary: "Test Execution for %s", project: {key: "%s"}}}) {testExecution {issueId jira(fields: ["key"])} warnings createdTestEnvironments}}`,
 		testId,
 		testJiraKey,
 		strings.Split(testJiraKey, "-")[0],
 	)
-	json := sendXrayQuery(s)
+	json, err := sendXrayQuery(s)
+	if err != nil {
+		log.Errorf("Failed to send Xray query err:%s\n", err)
+		return "", err
+	}
+
 	testExecId := gjson.Get(json, "data.createTestExecution.testExecution.issueId").Str
-	addTestExecutionToTestPlan(testPlanId, testExecId)
-	return testExecId
+	err = addTestExecutionToTestPlan(testPlanId, testExecId)
+	return testExecId, err
 }
 
-func DeleteTestExecution(run models.TestRun) {
+func DeleteTestExecution(run models.TestRun) error {
 	s := fmt.Sprintf(`mutation{deleteTestExecution(issueId: "%s")}`, run.TestExecIssueID)
-	sendXrayQuery(s)
+	_, err := sendXrayQuery(s)
+	return err
 }
 
-func addTestExecutionToTestPlan(testPlanId, testExecutionId string) {
+func addTestExecutionToTestPlan(testPlanId, testExecutionId string) error {
 	s := fmt.Sprintf(`mutation{addTestExecutionsToTestPlan(issueId: "%s", testExecIssueIds: ["%s"]) {addedTestExecutions warning}}`, testPlanId, testExecutionId)
-	sendXrayQuery(s)
+	_, err := sendXrayQuery(s)
+	return err
 }
