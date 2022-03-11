a019f5252e7f71b5b825f72d1c49cb5fe6cbc23d
Blaise Dias <blaise.dias@mayadata.io>
2022-03-11 16:03:16 +0000

    fix: make changes to run bolt weekly jobs
    Refactor to allow ubuntu LTS runs with different testplans for
     - bolt
     - mayastor
    
    Fix weekly pipline jobs to use testplans
    	- MQ-2743 for bolt
    	- MQ-2864 for mayastor (new)
    
    Move mayastor weekly and daily pipeline scripts
    to locations under pipelines/mayastor

--- pipelines/ubuntu-20.04-lts/ubuntu.groovy
+++ pipelines/ubuntu-20.04-lts/ubuntu.groovy
@@ -1,18 +1,23 @@
 #!/usr/bin/env groovy
 
-def GetDefaultJobParameters (k8sv, build_images, image_tag, datacore_bolt ) {
+// params must have the following fields
+//  xray_test_plan  : string
+//  build_images    : boolean
+//  image_tag       : string
+//  datacore_bolt   : boolean
+def GetDefaultJobParameters (k8sv, params) {
     return [
             [ $class: 'StringParameterValue',  name: "build_prefix",       value: "develop" ],
             [ $class: 'StringParameterValue',  name: "dataplaneBranch",    value: "develop" ],
             [ $class: 'StringParameterValue',  name: "controlplaneBranch", value: "develop" ],
-            [ $class: 'StringParameterValue',  name: "xray_test_plan",     value: "MQ-2743" ],
+            [ $class: 'StringParameterValue',  name: "xray_test_plan",     value: params.xray_test_plan ],
             [ $class: 'BooleanParameterValue', name: "xray_send_report",   value: true ],
             [ $class: 'StringParameterValue',  name: "test_profile",       value: "regression" ],
             [ $class: 'StringParameterValue',  name: "parallelism",        value: "10" ],
-            [ $class: 'StringParameterValue',   name: "kubernetes_version", value: k8sv ],
-            [ $class: 'BooleanParameterValue',  name: "build_images",       value: build_images ],
-            [ $class: 'StringParameterValue',   name: "image_tag",          value: image_tag ],
-            [ $class: 'BooleanParameterValue',  name: "datacore_bolt",      value: datacore_bolt ],
+            [ $class: 'StringParameterValue',  name: "kubernetes_version", value: k8sv ],
+            [ $class: 'BooleanParameterValue', name: "build_images",       value: params.build_images ],
+            [ $class: 'StringParameterValue',  name: "image_tag",          value: params.image_tag ],
+            [ $class: 'BooleanParameterValue', name: "datacore_bolt",      value: params.datacore_bolt ],
     ]
 }
 
